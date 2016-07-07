46c4da9c1d33fbb838aff2640e0cd39fe02007f1
miszcz <miszcz@miszcz>
2016-07-07 21:08:33 +0100

    VPC and subnet creation

--- environment/local/main.tf
+++ environment/local/main.tf
@@ -1,7 +1,14 @@
-variable "aws_region" {
-  default = "us-west-2"
-}
+variable "aws_region" {  default = "us-west-2" }
+variable "vpc_cidr"   { }
+
 
 provider "aws" {
 	region = "${var.aws_region}"
+}
+
+module "vpc" {
+  source = "../../modules/network"
+
+  vpc_cidr = "${var.vpc_cidr}"
+  
 }
---
+++ modules/network/vpc.tf
@@ -0,0 +1,18 @@
+variable "vpc_cidr" { }
+
+resource "aws_vpc" "development-vpc" {
+  cidr_block = "${var.vpc_cidr}"
+
+  tags {
+    Name = "development-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = "${aws_vpc.development-vpc.id}"
+    cidr_block = "7.7.7.0/25"
+
+    tags {
+        Name = "Main"
+    }
+}
