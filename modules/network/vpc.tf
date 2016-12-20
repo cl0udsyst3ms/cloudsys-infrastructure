9e58aeb9994c5b042d5c3587af51d94564d95a38
miszcz <miszcz@miszcz>
2016-12-20 23:49:26 +0000

    Add rds module - postgresql db

--- modules/network/vpc.tf
+++ modules/network/vpc.tf
@@ -1,5 +1,6 @@
 variable "vpc_cidr" { }
 variable "app_cidr" { default = "7.7.6.0/25,7.7.6.128/25" }
+variable "db_cidr"  { default = "7.7.7.128/26,7.7.7.192/26" }
 variable "AZs"      { }
 
 resource "aws_vpc" "development_vpc" {
@@ -21,6 +22,18 @@ resource "aws_subnet" "dmz_subnet" {
     }
 }
 
+resource "aws_subnet" "db_subnet" {
+    vpc_id     = "${aws_vpc.development_vpc.id}"
+    cidr_block = "${element(split(",", var.db_cidr), count.index)}"
+    availability_zone = "${element(split(",", var.AZs), count.index)}"
+    count             = "${length(split(",", var.db_cidr))}"
+
+    tags {
+        Name    = "db-subnet"
+        Creator = "terraform"
+    }
+}
+
 resource "aws_subnet" "app_subnet" {
     vpc_id     = "${aws_vpc.development_vpc.id}"
     cidr_block = "${element(split(",", var.app_cidr), count.index)}"
@@ -36,4 +49,5 @@ resource "aws_subnet" "app_subnet" {
 # Variables
 output "app_subnet_id"  { value = "${join(",", aws_subnet.app_subnet.*.id)}" }
 output "dmz_subnet_id"  { value = "${aws_subnet.dmz_subnet.id}" }
+output "db_subnet_id"   { value = "${join(",", aws_subnet.db_subnet.*.id)}" }
 output "vpc_id"         { value = "${aws_vpc.development_vpc.id}" }
